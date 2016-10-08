3b864eefa77d371c10c8ffe7c209bed961f06a5f
Ian Agpawa <agpawaji@gmail.com>
2016-10-08 01:36:25 -0400

    api request done, pick displays

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var https = require('https');
+var http = require('http');
 var bl = require('bl');
 
-
-
-
-
-
-
-// http://api.wunderground.com/api/241eebfb8057d10a/features/settings/q/query.format
 
 /* GET home page. */
 
 
 router.get('/', function(req, res, next) {
   var header = req.headers;
-  var ip = header['x-forwarded-for'] || process.env.TEST_IP
-  var weatherKey = process.env.UNDERGROUND
+  var ip = process.env.TEST_IP
+
+  var weatherKey = process.env.UNDERGROUND;
   var url = "http://api.wunderground.com/api/" + weatherKey + "/forecast/q/autoip.json?geo_ip=" + ip + ".json"
-console.log(url)
 
-/*
-
-  https.get('url', function (response){
+  console.log(url)
+  http.get(url, function (response){
     response.setEncoding('utf8');
 
     response.pipe(bl(function(err, data){
       if (err) throw err;
 
       var json = JSON.parse(data);
-      console.log(data)
-    }))
+      console.log(json)
+    })) //  response
+
+    //NEED TO res.render variables in here
 
 
-  })
+  }).on('error', function (e){
+    console.log("ERROR: "+ e.message)
+  }) // https
 
-*/
+
   res.render('index', { title: 'Local Weather', author: "Ian Agpawa", data: "We're getting there" });
 });
 
